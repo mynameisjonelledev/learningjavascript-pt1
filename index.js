1dbb5d0ef0f80bbc6f87ac91b19ee0368b7ac7dc
Jonel Cubio <jonelcubio021@gmail.com>
2024-10-01 20:23:42 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// This is a comment 
+// JavaScript comment 
+
+/* This
+  is
+  a
+  multiline
+  comment */
+
+
+  
+  console.log ('Items ${(8593 + 983290) / 100}');
+  console.log ('some' + 'text')
