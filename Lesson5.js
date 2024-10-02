916537bb3adf66a6179e416120cee5592e13109a
Jonel Cubio <jonelcubio021@gmail.com>
2024-10-02 19:29:53 +0800

    Add files via upload

---
+++ Lesson5.js
@@ -0,0 +1,26 @@
+console.log ('some' + 'text')
+
+  let calculation = 2 + 2;
+  console.log(calculation);
+
+  let var1 = 'Ling' + 'Xiao' + 'Yu'
+  console.log(var1);
+  
+
+  const name = 'Snacky';
+  console.log(`My name is ${name}`)
+
+  const cost = 5 + 2 * 3 + 9;
+  console.log(`Cost of food: $${cost}`);
+
+  const tax = cost * 0.1;
+
+  console.log(`Tax (10%): $${tax}`);
+
+  const totalCost = cost + tax;
+  console.log(`Total cost : $${totalCost}`);
+
+  // This is from cart quality //
+
+  let cartQuantity = 0;
+
