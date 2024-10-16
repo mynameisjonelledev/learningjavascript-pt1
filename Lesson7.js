9bfccfd141c3ff7411d3f8a913f41aefe71170b5
Jonel Cubio <jonelcubio021@gmail.com>
2024-10-16 20:07:37 +0800

    Update Lesson7.js

--- Lesson7.js
+++ Lesson7.js
@@ -122,8 +122,54 @@ function convertTemperature(degrees, unit) {
 console.log(convertTemperature(25, 'C'));
 console.log(convertTemperature(86, 'F'));
 
-/* To be honest*/
-/* Nalilito na pa ako sa return na 'yan.*/
 
 
-/* TAPUSIN MO UNG EXE */
+/* Lesson 7 Exercises - Functions */
+/* 7g */
+
+function convertLength(length, from , to) {
+  if (from === 'km' && to === 'miles') {
+    return `${length / 1.6} miles`;
+  } else if (from === 'miles' && to === 'km') {
+    return `${length * 1.6} km`
+  } else if (from === to) {
+    return `${length} ${to}`
+  }
+}
+
+console.log(convertLength(32, 'km' , 'miles'));
+console.log(convertLength(50, 'miles', 'km'));
+console.log(convertLength(1000, 'km', 'km'));
+
+
+/* 7h , 7i */
+
+function convertLength2(length, from , to) {
+  if (from === 'km' && to === 'miles') {
+    return `${length / 1.6} ${to}`;
+  } else if (from === 'km' && to === 'ft') {
+    return `${length * 3281} ${to}`;
+  } else if (from === 'miles' && to === 'km') {
+    return `${length * 1.6} ${to}`;
+  } else if (from === 'miles' && to === 'ft') {
+    return `${length * 5280} ${to}`;
+  } else if (from === 'ft' && to === 'km') {
+    return `${length /3281} ${to}`;
+  } else if (from === 'ft' && to === 'miles') {
+    return `${length /5280} ${to}`;
+  } else if (from === to) {
+    return `You are converting the same unit:${from && to}`;
+  } else if (from !== 'km' && from !== 'miles' && from !== 'ft') {
+    return `Invalid unit: $(from)`;
+  } else if (to !== 'km' && to !== 'miles' && to !== 'ft') {
+    return `Invalid unit: ${to}`;
+  }
+}
+
+console.log(convertLength2(5, 'miles', 'km'));
+console.log(convertLength2(5, 'miles', 'ft'));
+console.log(convertLength2(21, 'km', 'miles'));
+
+console.log(convertLength2(21, 'km', 'km'));
+
+console.log(convertLength2(23, 'miles', 'miles'));
