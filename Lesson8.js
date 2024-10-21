db2cd4ab34c6d592af8d812fbcfa7dd0c89df2dd
Jonel Cubio <jonelcubio021@gmail.com>
2024-10-21 18:47:57 +0800

    Update Lesson8.js

--- Lesson8.js
+++ Lesson8.js
@@ -138,3 +138,116 @@ if (basketballs.price < bowlingBall.price) {
 }
 
 console.log(comparePrice(basketballs, bowlingBall));
+
+
+// Lesson 8 Exercises - Objects //
+// 8e //
+
+const product10 = {
+  name: 'Keyboard',
+  price: 1253
+};
+
+const product11 = {
+  name: 'Keyboard',
+  price: 1253
+};
+
+const product12 = {
+  name: 'Gaming Mouse',
+  price: 1098
+}
+
+
+function isSameProduct(product1, product2) {
+  if (product10.name === product11.name && product10.price === product11.price) {
+    return true;
+  } else if (product11.name === product12.name && product11.price === product12.price) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isSameProduct(product10,product11));
+console.log(isSameProduct(product11, product10));
+
+// 8f //
+
+const cat1 = 'Snacky';
+const cat2 = 'Ling Xiao Yu'
+const greet = 'Good morning';
+
+console.log(cat1.toLowerCase());
+console.log(cat2.toUpperCase());
+console.log(greet.toLowerCase());
+
+// 8g //
+
+console.log('hello '.repeat(2));
+console.log('Meow '.repeat(3));
+
+
+// 8h //
+let calculation = localStorage.getItem('calculation') || '';
+
+function updateCalculation(value) {
+  calculation += value;
+  console.log(calculation);
+  localStorage.setItem('calculation', calculation);
+}
+
+
+
+
+/* const randomNumber = Math.random ();
+
+const result = ``;
+
+if (randomNumber < 0.5) {
+  result = `heads`;
+} else {
+  result = `tails`;
+}
+
+const guess = `heads`;
+
+if (guess === result) {
+  console.log(`You win!`);
+} else {
+  console.log(`Sorry, you lose!`);
+} */
+
+
+// Challenge Exercises //
+// 8i , 8j, 8k //
+
+const score2 = JSON.parse(localStorage.getItem('score2'))|| {
+  wins: 0,
+  losses: 0
+};
+
+function playGame(guess) {
+const randomNumber = Math.random ();
+
+let result = ``;
+
+
+if (randomNumber < 0.5) {
+  result = `heads`;
+} else {
+  result = `tails`;
+}
+
+
+if (guess === result) {
+  console.log('You win!');
+  score2.wins +=1;
+} else {
+  console.log('You lose!');
+  score2.losses +=1;
+}
+console.log(score2);
+
+localStorage.setItem('score2', JSON.stringify(score2));
+}
