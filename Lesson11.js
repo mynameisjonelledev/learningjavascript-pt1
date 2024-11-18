a3eb6cf3fc576bd3603cdd2f8831be6230ee1345
Jonel Cubio <jonelcubio021@gmail.com>
2024-11-18 16:02:05 +0800

    Update Lesson11.js

--- Lesson11.js
+++ Lesson11.js
@@ -1,4 +1,4 @@
-const todoList = [''];
+const todoList = [];
 
 renderTodoList ();
 
@@ -6,8 +6,22 @@ function renderTodoList () {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    
+    const { name, dueDate } = todoObject;
+
+    const html = 
+    `<div>${name}</div>
+     <div>${dueDate}</div>
+     
+     <button onclick="
+     todoList.splice(${i}, 1);
+     renderTodoList ();
+     ">Delete</button>
+    `;
     todoListHTML += html;
   }
   
@@ -20,8 +34,15 @@ function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
 
   const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+
+  const dueDate = dateInputElement.value;
   
-  todoList.push(name);
+  todoList.push({
+    name,
+    dueDate
+});
 
   inputElement.value = '';
 
