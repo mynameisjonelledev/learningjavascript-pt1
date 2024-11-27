dcb87ddd53a8d92e3e281cbe031e73b9a1f331d3
Jonel Cubio <jonelcubio021@gmail.com>
2024-11-27 21:54:09 +0800

    Update Lesson12.js

--- Lesson12.js
+++ Lesson12.js
@@ -48,6 +48,10 @@ function renderTodoList () {
   document.querySelector('.js-todo-list').innerHTML =  todoListHTML;
 }
 
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo();
+});
+
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
@@ -115,10 +119,14 @@ runTwice(add);
 //       1 second, the text inside the button changes to 
 //       'Finished!'
 
+document.querySelector('.js-start-finish').addEventListener('click', () => {
+  updateButton();
+});
+
 function startFinished() {
   const startButton = document.querySelector('.js-start-finish');
 
- /* setTimeout(function(){
+ /* setTimeout(() => {
     if(startButton.innerHTML === 'Start') {
       startButton.innerHTML = 'Finished'
       console.log('Finished')
@@ -126,7 +134,7 @@ function startFinished() {
   }, 1000); */
 
   // Simpler version from Simon
-    setTimeout(function() {
+    setTimeout(() => {
     startButton.innerHTML = 'Finished!';
   }, 1000);
 }
@@ -144,7 +152,7 @@ function updateButton() {
   button.classList.add('js-loading');
   button.innerHTML = 'Loading...';
   console.log('Loading...');
-  setTimeout(function() {
+  setTimeout(() => {
     button.classList.remove('js-loading');
     button.classList.add('js-finished');
     button.innerHTML = 'Finished!';
@@ -159,12 +167,16 @@ function updateButton() {
 //       button, display the message 'Added' below the button.
 //       Then,after 2 seconds, remove the message.
 
+document.querySelector('.js-add-to-cart').addEventListener('click', () => {
+  addedButton();
+});
+
 function addedButton() {  //✅
   const addedMessage = document.querySelector('.added-message');  //✅
 
   addedMessage.innerHTML = 'Added'; //✅
 
-  setTimeout(function(){  //✅
+  setTimeout(() => {  //✅
     addedMessage.innerHTML = '';  //✅
   }, 2000); //✅
 
@@ -191,7 +203,7 @@ function displayMessage() {
   // it doesn't remove the message too quickly.
   clearTimeout(timeoutId);
 
-  timeoutId = setTimeout(function() {
+  timeoutId = setTimeout(() => {
     messageElement.innerHTML = '';
   }, 2000);
 }
@@ -202,7 +214,7 @@ function displayMessage() {
 //       feature on the right (the title changes every 1 second).
 
 
-/* setInterval(function(){
+/* setInterval(() => {
 
   if (document.title === 'Lesson 12 : Advanced Functions') {
     document.title = `(${message}) New messages`;
@@ -216,6 +228,12 @@ function displayMessage() {
 // 12h - Continuing from 12g, instead of displaying 
 //       '(2) New messages' every time, save the number of messages
 //       in a variable: let messages = 2; 
+
+document.querySelector('.js-add-button').addEventListener('click', () => { addButton();
+});
+
+document.querySelector('.js-remove-button').addEventListener('click', () => { removeButton();
+});
 
 let message = 2;
 
@@ -236,7 +254,7 @@ function removeButton() {
 //       'App' as the title.
 //       When messages > 0 start changing the title again.
 
-setInterval(function(){
+setInterval(() => {
 
   if (document.title === 'Lesson 12 : Advanced Functions') {
    if (message === 0) {
@@ -249,3 +267,150 @@ setInterval(function(){
 
   }
 }, 1000);
+
+
+
+
+
+
+// what filter does is filter array
+([1, -3, 5].filter((value, index) => {
+  if(value >= 0){
+    return true;
+  } else {
+    return false;
+  }
+}));
+
+
+// what map does is create a new array
+[1,1,3].map((value,index) => {
+  return 10;
+});
+
+
+// 12j - Create an arrow function called 'multiply' that takes 2 numbers, multiplies them together, and returns the result.
+
+const multiply = (num1, num2) => {  //✅
+  return num1 * num2; //✅
+}
+
+console.log(multiply(2,3)); //✅
+
+
+//12k - Continuing from exercise 12j, write the arrow function
+//      on one line (if you already did this, skip this exercise).
+
+const multiply2 = (num1,num2) => num1 * num2; //✅
+
+console.log(multiply2(7,10)); //✅
+
+
+//12l - Create a function countPositive(nums) that takes an array
+//      of numbers and returns how many numbers in the array
+//      are greater than 0. Create this function using
+//      forEach() instead of a loop.
+
+function countPositive(nums) {
+  let positiveNumbers = 0;
+
+  // When there is only 1 parameter,
+  // the brackets are optional. So
+  // we can also write: num => { ... }
+  nums.forEach((num) => {
+    if (num > 0) {
+      positiveNumbers++;
+    }
+  });
+
+  return positiveNumbers;
+}
+
+console.log(countPositive([1, -3, 5]));
+console.log(countPositive([-2, 3, -5, 7, 10]));
+
+
+//12m - Create a function addNum(array,num) that takes an array
+//      of numbers and returns an array where each number is
+//      increased by 'num'. Create this function using .map()
+//      instead of a loop.
+
+function addNum(array,num) {
+  return array.map((value) => value + num);
+}
+
+console.log(addNum([1, 2, 3], 2));
+console.log(addNum([-2, -1, 0, 99], 2));
+
+
+//12n - Create a function removeEgg(foods) that takes an array
+//      of strings and returns an array where the strings 'egg'
+//      is removed. Create this function using .filter() instead
+//      of a loop.
+
+function removeEgg(foods) {
+  return foods.filter((food) => food !== 'egg');  
+}
+  
+console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
+
+
+//12o - Update exercise 12n to only remove the first 2 eggs from 
+//      the array. Again, use .filter() instead of a loop.
+
+
+
+function removeEgg(foods) { //✅
+
+  let eggsRemoved = 0;  //✅
+
+  return foods.filter((food) => { //✅
+    if (food === 'egg' && eggsRemoved < 2) {  //✅
+      eggsRemoved++;  //✅
+      return false; //✅
+    } 
+   
+    return true;  //✅
+  });
+}
+
+console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
+
+
+// 12p - Go back to the earlier exercises in lesson 12 (12c- 12i)
+//       and switch the code to use arrow functions. (When passing
+//       a function into another function, use arrow function.
+//       Otherwise, you can use either syntax).
+
+
+// 12q - Go back to the earlier exercises in lesson 12 (12c- 12i)
+//       and instead of using onclick="", switch the code to use 
+//       .addEventListener('click',...).
+
+
+
+
+// CHALLENGE EXERCISES 
+
+// 12r - Make a copy of the Rock Paper Scissors game into the files
+//       12r.html, 12r.css, 12r.js.
+
+// 12s - Update the 'Auto Play' button to use .addEventListener
+//       ('click', ...) instead of onclick="..".
+
+// 12t - Update the 'Auto Play' button so that when the game is 
+//       auto playing, the text in the button is 'Stop Playing'.
+//       Otherwise, the text in the button is 'Auto Play'.
+
+// 12u - Update the code so pressing 'a' on keyboard will auto
+//       play the game.
+
+// 12v - Update the 'Reset Score' button to use .addEventListener
+//       ('click' ,..).
+
+// 12w - Update the code so pressing 'Backspace' will reset the 
+//       score.
+
+// 12x - When clicking the 'Reset Score' or pressing 'Backspace',
+//       instead of resetting the score immediately, display a
+//       confirmation message below the button.
